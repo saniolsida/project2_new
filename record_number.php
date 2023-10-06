13efa887fe21a831f4d4f5350ac3df0f6169bebe
saniolsida <songsan3133@gmail.com>
2023-10-07 04:53:15 +0900

    make form in record

--- record_number.php
+++ record_number.php
@@ -17,3 +17,14 @@
     header("Location: index.html");
     exit;
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
