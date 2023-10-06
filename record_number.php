fcc776c6b06d531fb2df8a0af64b90161990205f
saniolsida <songsan3133@gmail.com>
2023-10-07 05:36:46 +0900

    add url

--- record_number.php
+++ record_number.php
@@ -4,6 +4,11 @@
     $gender = $_POST['gender'];
     $birthday = $_POST['birthday'];
     $group = $_POST['group'];
+    $url = 'index.html' . '?name=' . urlencode($name) . '&number=' . urlencode($number) . 
+    '&gender=' . urlencode($gender) . '&birthday=' . urlencode($birthday) . '&group=' . urlencode($group);
+    // Redirect to the index.html page with the parameters
+    header('Location: ' . $url);
+    exit;
 ?>
 <!DOCTYPE html>
 <html lang="en">
