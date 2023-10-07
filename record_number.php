8e0bb1662a93fba86d3f8960274c3426ed4da437
saniolsida <songsan3133@gmail.com>
2023-10-07 17:15:51 +0900

    Add css file

--- record_number.php
+++ record_number.php
@@ -1,19 +1,10 @@
 <?php
-$DB = new SQLite3('number_table.db');
-if($DB->lastErrorCode() != 0)
-    echo "Data connection failed"
-
     $name = $_POST['name'];
     $number = $_POST['number'];
     $gender = $_POST['gender'];
     $birthday = $_POST['birthday'];
     $group = $_POST['group'];
-    $DB->exec("INSERT INTO 'number_index' (name) VALUES ($name)");
-    $DB->exec("INSERT INTO 'number_index' (number) VALUES ($number)");
-    $DB->exec("INSERT INTO 'number_index' (gender) VALUES ($gender)");
-    $DB->exec("INSERT INTO 'number_index' (birthday) VALUES ($birthday)");
-    $DB->exec("INSERT INTO 'number_index' (group) VALUES ($group)");
-    $DB->exec("INSERT INTO 'number_index' (massage) VALUES ($message)");
+    $message = $_POST['message'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -21,7 +12,14 @@ if($DB->lastErrorCode() != 0)
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+      * {
+        font-size: 16px;
+        font-family: Consolas, sans-serif;
+      }
+    </style>
 </head>
 <body>
+    <p>Name is <?php echo $name ?>.</p>
 </body>
 </html>
